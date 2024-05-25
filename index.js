582e079e125209766de14306c1e64d1cf378d878
Fayssal Mechmeche <fayssalmechmeche.pro@gmail.com>
2024-05-25 12:13:33 +0200

    add base of project

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    console.log('Hello World!');
+    res.send('Hello World!');
+});
+
+
